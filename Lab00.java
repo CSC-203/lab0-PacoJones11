6f5bc9e8380d4ec3d3df187040cc13d2b2296eef
Paco Jones <ffjones2004@gmail.com>
2025-04-11 16:19:22 -0700

    Lab00

--- Lab00.java
+++ Lab00.java
@@ -2,11 +2,13 @@ public class Lab00
 {
    public static void main(String[] args)
    {
+      //initialize variables and print
       int x = 5;
       String y = "hello";
       float z = 9.8f;
       System.out.println("x:" + x + "y:" + y + "z:" + z);
 
+      //print numbers in a list
       int[] nums = {3, 6, -1, 2};
       for (int num : nums) {
          System.out.println(num);
